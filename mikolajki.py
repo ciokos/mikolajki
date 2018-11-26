b71f8b39360cdef73baa5b69e80d49a9dbe85515
Wojciech Ciok <33331400+ciokos@users.noreply.github.com>
2018-11-26 15:39:43 +0100

    Update mikolajki.py

--- mikolajki.py
+++ mikolajki.py
@@ -6,14 +6,6 @@ xd = l[:]
 xd.pop(8)
 xd = [a] + xd
 for x, y in zip(l, xd):
-    file2write = open(x + "2.0.txt", 'w')
-    file2write.write("Elo melo 420")
+    file2write = open(x + ".txt", 'w')
     file2write.write("Kup prezencik dla: " + y)
     file2write.close()
-
-
-
-
-
-
-
